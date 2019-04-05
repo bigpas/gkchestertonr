c96f6299f8517f8218b3a2e70e76620e5a471530
bigpas <pasqui.dente@gmail.com>
2019-04-06 00:12:50 +0100

    update: tests

--- tests/testthat/test_chesterton_books.R
+++ tests/testthat/test_chesterton_books.R
@@ -1,14 +1,14 @@
-# tests for austen_books function
+# tests for chesterton_books function
 
 context("Tidy dataframe for books")
 
 suppressPackageStartupMessages(library(dplyr))
 
-# test_that("factor order is correct", {
-#         d <- austen_books()
-#         expect_equal(levels(d$book)[1], "Heretics")
-#         expect_equal(levels(d$book)[6], "Man Alive")
-# })
+test_that("factor order is correct", {
+        d <- chesterton_books()
+        expect_equal(levels(d$book)[1], "Heretics")
+        expect_equal(levels(d$book)[2], "Orthodoxy")
+})
 
 test_that("get_book is correct", {
         orthodoxy <- gutenberg_download(130)$text
@@ -19,15 +19,15 @@ test_that("get_book is correct", {
                      )
 })
 
-# test_that("tidy frame for Chesterton books is right", {
-#         d <- austen_books() %>% 
-#                 group_by(book) %>%
-#                 summarise(total_lines = n())
-#         expect_equal(nrow(d), 6)
-#         expect_equal(ncol(d), 2)
-#         # the factor levels still in the right order?
-#         expect_equal(as.character(d$book[1]), "Heretics")
-#         expect_equal(as.character(d$book[6]), "Man Alive")
-#         # 
-#         expect_lt(d$total_lines[6], d$total_lines[4])
-# })
+test_that("tidy frame for Chesterton books is right", {
+        
+        d <- chesterton_books() %>%
+                group_by(book) %>%
+                summarise(total_lines = n())
+        
+        expect_equal(nrow(d), 11)
+        expect_equal(ncol(d), 2)
+        expect_equal(as.character(d$book[1]), "Heretics")
+        expect_equal(as.character(d$book[3]), "Man Alive")
+        expect_lt(d$total_lines[1], d$total_lines[3])
+})
