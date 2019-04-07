2144cb2dfd6dcd045d2343f0771f9ec0a63a1443
bigpas <pasqui.dente@gmail.com>
2019-04-07 23:19:14 +0100

    update: docs

---
+++ R/novels.R
@@ -0,0 +1,150 @@
+#' G.K. Chesterton's Complete Works.
+#' 
+#' This package contains the complete text of G.K. Chesterton's works, 
+#' formatted to be convenient for text analysis.
+#' @docType package
+#' @name gkchestertonr
+#' @aliases gkchestertonr gkchestertonr-package
+NULL
+
+#' The text of G.K. Chesterton's "Heretics"
+#'
+#' A dataset containing the text of Heretics.
+#' It is a collection of 20 essays originally published by 
+#' G.K. Chesterton in 1905. 
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/470}
+#' @format A character vector with xxx elements
+"heretics"
+
+#' The text of G.K. Chesterton's "Orthodoxy"
+#'
+#' A dataset containing the text of G.K. Chesterton's 1908, 
+#' a classic of Christian apologetics. 
+#' The book was written when Chesterton was an Anglican. 
+#' 14 years later he converted to Catholicism. 
+#' Chesterton chose the title, Orthodoxy, 
+#' to focus instead on the plainness of the Apostles' Creed.
+#' 
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/130}
+#' @format A character vector with xxx elements
+"orthodoxy"
+
+#' The text of G.K. Chesterton's novel "Man Alive"
+#'
+#' A dataset containing the text of G.K. Chesterton's 1912 novel "Man Alive". 
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/1718}
+#' @format A character vector with xxx elements
+"manalive"
+
+#' The text of G.K. Chesterton's novel ""
+#'
+#' A dataset containing the text of G.K. Chesterton's 1815 novel "Emma".
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each. 
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/158}
+#' @format A character vector with xxx elements
+"thenewjerusalem"
+
+#' The text of G.K. Chesterton's novel The Man Who Was Thursday: A Nightmare
+#'
+#' A dataset containing the text of G.K. Chesterton's novel 
+#' "The Man Who Was Thursday: A Nightmare", 
+#' first published in 1908
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/1695}
+#' @format A character vector with xxx elements
+"themanwhowasthursday"
+
+#' The text of G.K. Chesterton's novel "What's Wrong with the World"
+#'
+#' A dataset containing the text of G.K. Chesterton's collection of essays 
+#' "What's Wrong with the World", published in 1910.
+#' A collection of classic essays by the inimitable G.K. Chesterton, 
+#' exploring a variety of topics.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/1717}
+#' @format A character vector with xxx elements
+"whatswrongwiththeworld"
+
+#' The text of G.K. Chesterton's novel "The Napoleon of Nothing Hill"
+#'
+#' A dataset containing the text of G.K. Chesterton's novel 
+#' "The Napoleon of Nothing Hill", published in 1904.
+
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/20058}
+#' @format A character vector with xxx elements
+"thenapoleonofnothinghill"
+
+#' The text of G.K. Chesterton's "Eugenics and other Evils"
+#'
+#' A dataset containing the text of G.K. Chesterton's "Eugenics and other Evils", 
+#' published in 1922.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/25308}
+#' @format A character vector with xxx elements
+"eugenicsandotherevils"
+
+#' The text of G.K. Chesterton's "A Short History of England"
+#'
+#' A dataset containing the text of G.K. Chesterton's 
+#' "A Short History of England", 
+#' published in 1917.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/20897}
+#' @format A character vector with xxx elements
+"ashorthistoryofengland"
+
+#' The text of G.K. Chesterton's "The Man Who Knew too much"
+#'
+#' A dataset containing the text of G.K. Chesterton's detective story 
+#' "The Man Who Knew too much", 
+#' published in 1922.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/1720}
+#' @format A character vector with xxx elements
+"themanwhoknewtoomuch"
+
+#' The text of G.K. Chesterton's "The Ballad of the White Horse"
+#'
+#' A dataset containing the text of G.K. Chesterton's  
+#' "The Ballad of the White Horse", published in 1911.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/1719}
+#' @format A character vector with xxx elements
+"theballadofthewhitehorse"
